d70297658eb7299d01b000692a2c1660e145412a
mariusz.repczynski <mariuszrepczynski@gmail.com>
2024-12-10 15:36:46 +0000

    removed log in button fom navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -99,12 +99,7 @@ export default function Navbar({ onMenuClick }: NavbarProps) {
               )}
             </div>
           ) : (
-            <button
-              onClick={() => window.history.pushState(null, '', '/login')}
-              className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors"
-            >
-              Login
-            </button>
+            <div></div>
           )}
         </div>
       </div>
